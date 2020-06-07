15e1fc026794fc95405afea77ba7d943ac2206a2
mikah0 <66323215+mikah0@users.noreply.github.com>
2020-06-07 18:47:59 -0400

    Add files via upload

---
+++ AWD_W4.atwo.py
@@ -0,0 +1,27 @@
+import urllib
+from bs4 import BeautifulSoup
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+#line 1-8 is copy paste from lecture vidoes ^^
+
+url = input("Enter URL- ")
+position = int(input("Enter position: "))
+count = int(input("Enter count: "))
+
+#promt user to enter url(link), the position where to extract the url and
+#the number of times to repeat this cycle ("count").
+
+for i in range(count): #range function = range(start, stop, step)
+    html = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(html)
+    tags = soup('a')
+    url = tags[position-1].get("href")
+    result = tags[position-1].string
+
+print(result)
+
+#Answer 'RICE'
